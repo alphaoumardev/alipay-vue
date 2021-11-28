475582eae632b43ecbd181b255059a50df3bfdc2
oumar <oumardialo98@qq.com>
2021-11-28 19:54:09 +0800

    first

---
+++ src/test/java/com/alpha/alipay/AlipayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alpha.alipay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlipayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
